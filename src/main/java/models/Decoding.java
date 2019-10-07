e405fd248205e1e4b835d492b22c09b855ee22c5
atembamanu <atembaemm07@gmail.com>
2019-10-07 14:43:48 +0300

    initialize constructor and getters and setters for the Decoding class

--- src/main/java/models/Decoding.java
+++ src/main/java/models/Decoding.java
@@ -1,4 +1,27 @@
 package models;
 
 public class Decoding {
+    String sentence;
+    int theKey;
+
+    public Decoding(String sentence, int theKey) {
+        this.sentence = sentence;
+        this.theKey = theKey;
+    }
+
+    public String getSentence() {
+        return sentence;
+    }
+
+    public void setSentence(String sentence) {
+        this.sentence = sentence;
+    }
+
+    public int getTheKey() {
+        return theKey;
+    }
+
+    public void setTheKey(int theKey) {
+        this.theKey = theKey;
+    }
 }
