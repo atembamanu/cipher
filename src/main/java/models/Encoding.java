ad15e3ad89926ab86eac7b4b08f0f3563450d14d
atembamanu <atembaemm07@gmail.com>
2019-10-07 14:30:40 +0300

    Initialize a constructor which sentence and key as parameters

--- src/main/java/models/Encoding.java
+++ src/main/java/models/Encoding.java
@@ -1,4 +1,10 @@
 package models;
 
 public class Encoding {
+    String sentence;
+    int theKey;
+    public Encoding(String sentence, int theKey) {
+        this.sentence = sentence;
+        this.theKey = theKey;
+    }
 }
